e9e2ca8d9dc06cd279171f1cbaa3ee5050b8a078
Meghashree N <meghashreetendulkar@gmail.com>
2022-10-17 11:54:17 +0530

    Update bucket.tf

--- bucket.tf
+++ bucket.tf
@@ -1,95 +1,27 @@
-resource "oci_objectstorage_bucket" "test_bucket" {
+resource "oci_objectstorage_bucket" "product_bucket" {
     #Required
     compartment_id = oci_identity_compartment.compartmentProduct.id
-    name = var.bucket_name
+    name = "Product_Bucket"
     namespace = var.bucket_namespace
-
-    #Optional
-    access_type = var.bucket_access_type
-    auto_tiering = var.bucket_auto_tiering
-    metadata = var.bucket_metadata
-    object_events_enabled = var.bucket_object_events_enabled
-    storage_tier = var.bucket_storage_tier
-    retention_rules {
-        display_name = var.retention_rule_display_name
-        duration {
-            #Required
-            time_amount = var.retention_rule_duration_time_amount
-            time_unit = var.retention_rule_duration_time_unit
-        }
-        time_rule_locked = var.retention_rule_time_rule_locked
-    }
-    versioning = var.bucket_versioning
 }
 
-resource "oci_objectstorage_bucket" "test_bucket" {
+resource "oci_objectstorage_bucket" "customer_bucket" {
     #Required
     compartment_id = oci_identity_compartment.compartmentCustomer.id
-    name = var.bucket_name
+    name = "Customer_Bucket"
     namespace = var.bucket_namespace
-
-    #Optional
-    access_type = var.bucket_access_type
-    auto_tiering = var.bucket_auto_tiering
-    metadata = var.bucket_metadata
-    object_events_enabled = var.bucket_object_events_enabled
-    storage_tier = var.bucket_storage_tier
-    retention_rules {
-        display_name = var.retention_rule_display_name
-        duration {
-            #Required
-            time_amount = var.retention_rule_duration_time_amount
-            time_unit = var.retention_rule_duration_time_unit
-        }
-        time_rule_locked = var.retention_rule_time_rule_locked
-    }
-    versioning = var.bucket_versioning
 }
 
-resource "oci_objectstorage_bucket" "test_bucket" {
+resource "oci_objectstorage_bucket" "sales_bucket" {
     #Required
     compartment_id = oci_identity_compartment.compartmentSales.id
-    name = var.bucket_name
+    name = "Sales_Bucket"
     namespace = var.bucket_namespace
-
-    #Optional
-    access_type = var.bucket_access_type
-    auto_tiering = var.bucket_auto_tiering
-    metadata = var.bucket_metadata
-    object_events_enabled = var.bucket_object_events_enabled
-    storage_tier = var.bucket_storage_tier
-    retention_rules {
-        display_name = var.retention_rule_display_name
-        duration {
-            #Required
-            time_amount = var.retention_rule_duration_time_amount
-            time_unit = var.retention_rule_duration_time_unit
-        }
-        time_rule_locked = var.retention_rule_time_rule_locked
-    }
-    versioning = var.bucket_versioning
 }
 
-resource "oci_objectstorage_bucket" "test_bucket" {
+resource "oci_objectstorage_bucket" "dataOps_bucket" {
     #Required
     compartment_id = oci_identity_compartment.compartmentDataOps.id
-    name = var.bucket_name
+    name = "DataOps_bucket"
     namespace = var.bucket_namespace
-
-    #Optional
-    access_type = var.bucket_access_type
-    auto_tiering = var.bucket_auto_tiering
-    metadata = var.bucket_metadata
-    object_events_enabled = var.bucket_object_events_enabled
-    storage_tier = var.bucket_storage_tier
-    retention_rules {
-        display_name = var.retention_rule_display_name
-        duration {
-            #Required
-            time_amount = var.retention_rule_duration_time_amount
-            time_unit = var.retention_rule_duration_time_unit
-        }
-        time_rule_locked = var.retention_rule_time_rule_locked
-    }
-    versioning = var.bucket_versioning
 }
