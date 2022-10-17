3ae11314a8caa1d0ce440b1a3e864a711fc4846b
Meghashree N <meghashreetendulkar@gmail.com>
2022-10-17 10:03:16 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,15 @@
+#configuration variables
+
+variable "tenancy_ocid" {}
+variable "compartment_ocid" {}
+variable "user_ocid" {}
+variable "fingerprint" {}
+variable "private_key_path" {}
+variable "region" {}
+variable "region2" {}
+
+#declare oci providers
+
 provider "oci"{
     alias  = "home"
     tenancy_ocid = var.tenancy_ocid
@@ -7,10 +19,13 @@ provider "oci"{
     fingerprint = var.fingerprint
 }
 
-variable "tenancy_ocid" {}
-variable "compartment_ocid" {}
-variable "user_ocid" {}
-variable "fingerprint" {}
-variable "private_key_path" {}
-variable "region" {}
+provider "oci" {
+  alias            = "region2"
+  tenancy_ocid     = var.tenancy_ocid
+  fingerprint      = var.fingerprint
+  private_key_path = var.private_key_path
+  region           = var.region2
+  user_ocid        = var.user_ocid
+}
+
 
