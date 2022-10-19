2b60a23b7816d01978f04bfb2b900d64f61ef694
Meghashree N <meghashreetendulkar@gmail.com>
2022-10-19 15:08:53 +0530

    Update object_storage.tf

--- object_storage.tf
+++ object_storage.tf
@@ -40,13 +40,7 @@ esource "oci_objectstorage_object" "product_object" {
     object = var.object_object
 
     #Optional
-    cache_control = var.object_cache_control
-    content_disposition = var.object_content_disposition
     content_encoding = var.object_content_encoding
-    content_language = var.object_content_language
     content_type = var.object_content_type
-    delete_all_object_versions = var.object_delete_all_object_versions
-    metadata = var.object_metadata
     storage_tier = var.object_storage_tier
-    opc_sse_kms_key_id = var.object_opc_sse_kms_key_id
 }
