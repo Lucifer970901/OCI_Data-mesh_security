983cdb1b7acd20a365d2417b5aa55616e914030f
Meghashree N <meghashreetendulkar@gmail.com>
2022-10-20 12:15:34 +0530

    Update and rename dataflow.txt to dataflow.tf

--- dataflow.tf
+++ dataflow.tf
@@ -1,27 +1,26 @@
+resource "oci_dataflow_private_endpoint" "test_private_endpoint" {
+    #Required
+    compartment_id = var.compartment_id
+    dns_zones = var.private_endpoint_dns_zones
+    subnet_id = oci_core_subnet.publicsubnet.id
+
+    #Optional
+    display_name = var.private_endpoint_display_name
+    max_host_count = var.private_endpoint_max_host_count
+    nsg_ids = var.private_endpoint_nsg_ids
+}
+
 resource "oci_dataflow_application" "test_application" {
     #Required
     compartment_id = oci_identity_compartment.compartmentProduct.id
     display_name = var.application_display_name
     driver_shape = var.application_driver_shape
     executor_shape = var.application_executor_shape
-    file_uri = var.application_file_uri
+    file_uri = oci_objectstorage_object.product_object.source_uri_details
     language = var.application_language
     num_executors = var.application_num_executors
     spark_version = var.application_spark_version
-
-    #Optional
-    application_log_config {
-        #Required
-        log_group_id = oci_logging_log_group.test_log_group.id
-        log_id = oci_logging_log.test_log.id
-    }
-    
-    archive_uri = var.application_archive_uri
-    arguments = var.application_arguments
-    class_name = var.application_class_name
     configuration = var.application_configuration
-    defined_tags = {"Operations.CostCenter"= "42"}
-    description = var.application_description
     driver_shape_config {
 
         #Optional
@@ -35,15 +34,9 @@ resource "oci_dataflow_application" "test_application" {
         memory_in_gbs = var.application_executor_shape_config_memory_in_gbs
         ocpus = var.application_executor_shape_config_ocpus
     }
-    freeform_tags = {"Department"= "Finance"}
-    logs_bucket_uri = var.application_logs_bucket_uri
-    metastore_id = var.metastore_id
-    parameters {
-        #Required
-        name = var.application_parameters_name
-        value = var.application_parameters_value
-    }
+
+  
     private_endpoint_id = oci_dataflow_private_endpoint.test_private_endpoint.id
     type = var.application_type
-    warehouse_bucket_uri = var.application_warehouse_bucket_uri
+    
 }
