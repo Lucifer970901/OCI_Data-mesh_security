c78dc44f7c2e6f6e4b543076eb750706c99b201a
Meghashree N <meghashreetendulkar@gmail.com>
2022-10-20 11:42:55 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,8 @@
 output "object_data" {
   value = <<EOF
-  content = ${data.oci_objectstorage_object.product_object.content}
-  content-length = ${data.oci_objectstorage_object.product_object.content_length}
-  content-type = ${data.oci_objectstorage_object.product_object.content_type}
+  content = ${oci_objectstorage_object.product_object.content}
+  content-length = ${oci_objectstorage_object.product_object.content_length}
+  content-type = ${oci_objectstorage_object.product_object.content_type}
 EOF
 
 }
