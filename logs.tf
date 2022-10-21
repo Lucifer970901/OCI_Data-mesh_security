4dacf2368b261658c4d850ffe1a58b607f26029e
Meghashree N <meghashreetendulkar@gmail.com>
2022-10-21 14:28:02 +0530

    Create logs.tf

---
+++ logs.tf
@@ -0,0 +1,29 @@
+resource "oci_logging_log_group" "product_log_group" {
+    #Required
+    compartment_id = oci_identity_compartment.productCompartment.id
+    display_name = var.log_group_display_name
+}
+
+resource "oci_logging_log" "test_log" {
+    #Required
+    display_name = "Product_log"
+    log_group_id = oci_logging_log_group.product_log_group.id
+    log_type = var.log_log_type
+
+    #Optional
+    #configuration {
+        #Required
+    #    source {
+            #Required
+    #        category = var.log_configuration_source_category
+     #       resource = var.log_configuration_source_resource
+      #      service = var.log_configuration_source_service
+       #     source_type = "ociservice"
+        #}
+
+        #Optional
+        compartment_id = oci_identity_compartment.productCompartment.id
+    }
+    is_enabled = "enabled"
+    retention_duration = "60"
+}
