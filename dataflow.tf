83376669de44fa5883d499b0e16b946e9249659b
Meghashree N <meghashreetendulkar@gmail.com>
2022-10-21 20:27:04 +0530

    Update dataflow.tf

--- dataflow.tf
+++ dataflow.tf
@@ -10,23 +10,78 @@
     #nsg_ids = var.private_endpoint_nsg_ids
 #}
 
-resource "oci_dataflow_application" "test_application" {
+resource "oci_dataflow_application" "product_application" {
     #Required
     compartment_id = oci_identity_compartment.compartmentProduct.id
-    display_name = var.application_display_name
+    display_name = var.product_application_display_name
     driver_shape = var.application_driver_shape
     executor_shape = var.application_executor_shape
-    file_uri = var.application_file_uri
+    file_uri = var.product_application_file_uri
     language = var.application_language
     num_executors = var.application_num_executors
     spark_version = var.application_spark_version
-    logs_bucket_uri = var.application_logs_bucket_uri
- #   application_log_config {
-        #Required
- #       log_group_id = oci_logging_log_group.test_log_group.id
- #       log_id = oci_logging_log.test_log.id
- #   }
+    logs_bucket_uri = var.product_application_logs_bucket_uri
+    driver_shape_config {
 
+        #Optional
+        memory_in_gbs = var.application_driver_shape_config_memory_in_gbs
+        ocpus = var.application_driver_shape_config_ocpus
+    }
+    #execute = var.application_execute
+    executor_shape_config {
+
+        #Optional
+        memory_in_gbs = var.application_executor_shape_config_memory_in_gbs
+        ocpus = var.application_executor_shape_config_ocpus
+    }
+
+  
+   # private_endpoint_id = oci_dataflow_private_endpoint.test_private_endpoint.id
+   # type = var.application_type
+   
+}
+
+resource "oci_dataflow_application" "customer_application" {
+    #Required
+    compartment_id = oci_identity_compartment.compartmentCustomer.id
+    display_name = var.product_application_display_name
+    driver_shape = var.application_driver_shape
+    executor_shape = var.application_executor_shape
+    file_uri = var.customer_application_file_uri
+    language = var.application_language
+    num_executors = var.application_num_executors
+    spark_version = var.application_spark_version
+    logs_bucket_uri = var.customer_application_logs_bucket_uri
+    driver_shape_config {
+
+        #Optional
+        memory_in_gbs = var.application_driver_shape_config_memory_in_gbs
+        ocpus = var.application_driver_shape_config_ocpus
+    }
+    #execute = var.application_execute
+    executor_shape_config {
+
+        #Optional
+        memory_in_gbs = var.application_executor_shape_config_memory_in_gbs
+        ocpus = var.application_executor_shape_config_ocpus
+    }
+
+  
+   # private_endpoint_id = oci_dataflow_private_endpoint.test_private_endpoint.id
+   # type = var.application_type
+   
+}
+resource "oci_dataflow_application" "sales_application" {
+    #Required
+    compartment_id = oci_identity_compartment.compartmentSales.id
+    display_name = var.product_application_display_name
+    driver_shape = var.application_driver_shape
+    executor_shape = var.application_executor_shape
+    file_uri = var.sales_application_file_uri
+    language = var.application_language
+    num_executors = var.application_num_executors
+    spark_version = var.application_spark_version
+    logs_bucket_uri = var.sales_application_logs_bucket_uri
     driver_shape_config {
 
         #Optional
