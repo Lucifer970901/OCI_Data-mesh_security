2409048a597aef4964c3c4cb6d3fecf86ec4f0a0
Meghashree N <meghashreetendulkar@gmail.com>
2022-10-21 20:39:30 +0530

    Update VCN.tf

--- VCN.tf
+++ VCN.tf
@@ -21,11 +21,35 @@ resource "oci_core_subnet" "publicsubnet"{
     security_list_ids = [oci_core_security_list.publicSL.id]
 }
 
+#resource block for defining private subnet
+resource "oci_core_subnet" "privatesubnet"{
+    provider = oci.region2
+    dns_label = var.privateSubnet_dns_label
+    compartment_id = oci_identity_compartment.compartmentNetworking.id
+    vcn_id = oci_core_vcn.test_vcn.id
+    display_name = var.display_name_privatesubnet
+    cidr_block = var.cidr_block_privatesubnet
+    route_table_id = oci_core_route_table.privateRT.id
+    security_list_ids = [oci_core_security_list.privateSL.id]
+}
+
 #resource block for internet gateway
 resource "oci_core_internet_gateway" "test_internet_gateway" {
     provider = oci.region2
     compartment_id = oci_identity_compartment.compartmentNetworking.id
     vcn_id         = oci_core_vcn.test_vcn.id
+}
+
+resource "oci_core_nat_gateway" "test_nat_gateway" {
+    #Required
+    compartment_id = oci_identity_compartment.compartmentNetworking.id
+    vcn_id = oci_core_vcn.test_vcn.id
+
+    #Optional
+    block_traffic = var.nat_gateway_block_traffic
+  
+    public_ip_id = oci_core_public_ip.test_public_ip.id
+    route_table_id = oci_core_route_table.test_route_table.id
 }
 
 #resource block for route table with route rule for internet gateway
@@ -37,6 +61,18 @@ resource "oci_core_route_table" "publicRT" {
   route_rules {
     destination       = "0.0.0.0/0"
     network_entity_id = oci_core_internet_gateway.test_internet_gateway.id
+  }
+}
+
+#resource block for route table with route rule for nat gateway
+resource "oci_core_route_table" "privateRT" {
+    provider = oci.region2
+    vcn_id = oci_core_vcn.test_vcn.id
+    compartment_id = oci_identity_compartment.compartmentNetworking.id
+
+  route_rules {
+    destination       = "0.0.0.0/0"
+    network_entity_id = oci_core_nat_gateway.test_nat_gateway.id
   }
 }
 
@@ -76,6 +112,53 @@ resource "oci_core_security_list" "publicSL" {
 
     protocol = "1"
     source   = "0.0.0.0/0"
+  }
+  ingress_security_rules {
+    icmp_options {
+      type = "8"
+    }
+
+    protocol = "1"
+    source   = "0.0.0.0/0"
+  }
+}
+
+#resource block for security list
+resource "oci_core_security_list" "privateSL" {
+    provider = oci.region2
+    compartment_id = oci_identity_compartment.compartmentNetworking.id
+    vcn_id         = oci_core_vcn.test_vcn.id
+    display_name   = "private_security_list"
+
+  egress_security_rules {
+    protocol    = "all"
+    destination = "0.0.0.0/0"
+  }
+  ingress_security_rules {
+    tcp_options {
+      max = "22"
+      min = "22"
+    }
+
+    protocol = "6"
+    source   = var.vcn_cidr_block
+  }
+  ingress_security_rules {
+    icmp_options {
+      type = "0"
+    }
+
+    protocol = "1"
+    source   = "0.0.0.0/0"
+  }
+  ingress_security_rules {
+    icmp_options {
+      type = "3"
+      code = "4"
+    }
+
+    protocol = "1"
+    source   = var.vcn_cidr_block
   }
   ingress_security_rules {
     icmp_options {
