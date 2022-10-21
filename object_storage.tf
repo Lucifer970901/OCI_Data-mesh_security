7f69423c0aeea37d1853a2b984b9472d3d22f6d9
Meghashree N <meghashreetendulkar@gmail.com>
2022-10-21 20:16:15 +0530

    Update object_storage.tf

--- object_storage.tf
+++ object_storage.tf
@@ -1,9 +1,14 @@
+data "oci_objectstorage_namespace" "ns" {
+  #Optional
+  compartment_id = oci_identity_compartment.compartmentProduct.id
+}
+
 #resources bucket
 resource "oci_objectstorage_bucket" "product_bucket" {
     #Required
     compartment_id = oci_identity_compartment.compartmentProduct.id
     name = "Product_Bucket"
-    namespace = var.bucket_namespace
+    namespace = data.oci_objectstorage_namespace.ns.namespace
     access_type = var.bucket_access_type
 }
 
@@ -11,7 +16,7 @@ resource "oci_objectstorage_bucket" "customer_bucket" {
     #Required
     compartment_id = oci_identity_compartment.compartmentCustomer.id
     name = "Customer_Bucket"
-    namespace = var.bucket_namespace
+    namespace = data.oci_objectstorage_namespace.ns.namespace
     access_type = var.bucket_access_type
 }
 
@@ -19,7 +24,7 @@ resource "oci_objectstorage_bucket" "sales_bucket" {
     #Required
     compartment_id = oci_identity_compartment.compartmentSales.id
     name = "Sales_Bucket"
-    namespace = var.bucket_namespace
+    namespace = data.oci_objectstorage_namespace.ns.namespace
     access_type = var.bucket_access_type
 }
 
@@ -31,18 +36,44 @@ resource "oci_objectstorage_bucket" "dataOps_bucket" {
     access_type = var.bucket_access_type
 }
 
-#resources object
+#resources product object
 resource "oci_objectstorage_object" "product_object" {
     #Required
     bucket =oci_objectstorage_bucket.product_bucket.name
     content = file(var.object_content)
-    namespace = var.object_namespace
+    namespace = data.oci_objectstorage_namespace.ns.namespace
     object = var.object_object
 
     #Optional
     storage_tier = var.object_storage_tier
-    #content_type = var.object_content_type
-    #content_type        = "text/x-python-script"
-    content_disposition = "attachment; filename=\"transform_script_data_transform_script_hardcoded.py\""
+    content_disposition = "attachment; filename=\"data_transform.py\""
+    content_encoding = filemd5(var.object_content)
+}
+
+#resources product object
+resource "oci_objectstorage_object" "customer_object" {
+    #Required
+    bucket =oci_objectstorage_bucket.customer_bucket.name
+    content = file(var.object_content)
+    namespace = data.oci_objectstorage_namespace.ns.namespace
+    object = var.object_object
+
+    #Optional
+    storage_tier = var.object_storage_tier
+    content_disposition = "attachment; filename=\"data_transform.py\""
+    content_encoding = filemd5(var.object_content)
+}
+
+#resources product object
+resource "oci_objectstorage_object" "sales_object" {
+    #Required
+    bucket =oci_objectstorage_bucket.sales_bucket.name
+    content = file(var.object_content)
+    namespace = data.oci_objectstorage_namespace.ns.namespace
+    object = var.object_object
+
+    #Optional
+    storage_tier = var.object_storage_tier
+    content_disposition = "attachment; filename=\"data_transform.py\""
     content_encoding = filemd5(var.object_content)
 }
